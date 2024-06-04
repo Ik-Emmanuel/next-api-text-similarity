24ac0ad884fc20c3acbca73875249cfc536950c2
Emmauel Ikechukwu <eik@pml.ac.uk>
2024-06-04 20:44:46 +0100

    add documentation page and code animation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,8 +11,8 @@ const Navbar = async () => {
   const session = undefined;
 
   return (
-    <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
-      <div className="container max-w-7xl  w-full flex justify-between items-center mx-10">
+    <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-light-gold shadow-sm flex items-center justify-between">
+      <div className="container max-w-7xl  w-full flex justify-between items-center mx-auto">
         <Link href="/" className={buttonVariants({ variant: "link" })}>
           <span className="hover:no-underline">Text Similarity v1.0</span>
         </Link>
